e33eb11ba5fcdd95cda57ddb59f91a144f018161
crazystudent13 <1076535690@qq.com>
2022-12-06 16:02:06 +0800

    移除时间格式化，之后使用dayJS中的时间格式化

--- src/time/index.js
+++ src/time/index.js
@@ -7,28 +7,6 @@ export function compareDate(startTime, endTime) {
   return flag
 }
 
-// 时间格式化插件
-export function formatTime(date, fmt) {
-  let newTime = new Date(date)
-  let ret
-  const opt = {
-    'Y+': newTime.getFullYear().toString(), // 年
-    'm+': (newTime.getMonth() + 1).toString(), // 月
-    'd+': newTime.getDate().toString(), // 日
-    'H+': newTime.getHours().toString(), // 时
-    'M+': newTime.getMinutes().toString(), // 分
-    'S+': newTime.getSeconds().toString(), // 秒
-    // 有其他格式化字符需求可以继续添加，必须转化成字符串
-  }
-  for (let k in opt) {
-    ret = new RegExp('(' + k + ')').exec(fmt)
-    if (ret) {
-      fmt = fmt.replace(ret[1], ret[1].length == 1 ? opt[k] : opt[k].padStart(ret[1].length, '0'))
-    }
-  }
-  return fmt
-}
-
 /**
  * 时间戳转换工具
  * @constructor
